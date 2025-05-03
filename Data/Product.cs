640652c988047605543fabbf4423a4b41710e179
MitaKa7 <mitkot2007@gmail.com>
2025-05-03 14:32:53 +0300

    Added validations for Product

--- Data/Product.cs
+++ Data/Product.cs
@@ -8,21 +8,25 @@ namespace AuthOnlineApp.Data
     {
         public int ProductId { get; set; }
 
-        
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, ErrorMessage = "Product name cannot be longer than 100 characters.")]
         public string Name { get; set; }
 
-        
+        [StringLength(1000, ErrorMessage = "Description cannot be longer than 1000 characters.")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Starting price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Starting price must be greater than zero.")]
         [Precision(18, 2)]
         public decimal StartingPrice { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        [Required(ErrorMessage = "CreatedByUserId is required.")]
         public string CreatedByUserId { get; set; }
+
         public virtual ApplicationUser? CreatedByUser { get; set; }
 
         public virtual ICollection<Bid>? Bids { get; set; }
-
     }
 }

bcca5b9cfe329871012b093bafa02165d641ba45
MitaKa7 <mitkot2007@gmail.com>
2025-05-03 16:49:33 +0300

    Added search box in home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,38 @@
 using System.Diagnostics;
+using AuthOnlineApp.Data;
 using AuthOnlineApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AuthOnlineApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string? searchString)
         {
-            return View();
+            ViewData["CurrentFilter"] = searchString;
+
+            var products = from p in _context.Product
+                           select p;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Name.Contains(searchString));
+            }
+
+            return View(await products.ToListAsync());
         }
+
 
         public IActionResult Privacy()
         {
